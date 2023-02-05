c8d56502f5cab04dcadb6135357c74cf47902b26
nozomanai <78668683+nozomanai@users.noreply.github.com>
2023-02-05 22:35:35 +0800

    Create anykernel.sh

---
+++ anykernel.sh
@@ -0,0 +1,39 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Evasi0nKernel
+do.devicecheck=1
+do.modules=0
+do.systemless=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=cepheus
+device.name2=Cepheus
+device.name3=cepheus-user
+device.name4=Mi 9
+device.name5=Mi9
+supported.versions=12
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=auto;
+is_slot_device=auto;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+
+## AnyKernel install
+split_boot;
+
+mv $home/rd-new.cpio $home/ramdisk-new.cpio
+
+flash_boot;
+## end install
