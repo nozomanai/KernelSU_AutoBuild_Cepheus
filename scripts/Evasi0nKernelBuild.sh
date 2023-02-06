b8ecde3de6e8aec1c44e5dd8d56407ff1fe227b7
nozomanai <78668683+nozomanai@users.noreply.github.com>
2023-02-06 09:06:54 +0800

    Create Evasi0nKernelBuild.sh

---
+++ scripts/Evasi0nKernelBuild.sh
@@ -0,0 +1,25 @@
+echo download kernel source
+cd $GITHUB_WORKSPACE && mkdir kernel_workspace && cd kernel_workspace
+git clone ${{ github.event.inputs.KERNEL_SOURCE }} -b ${{ github.event.inputs.KERNEL_SOURCE_BRANCH }} android-kernel --depth=1
+
+echo delete yaml scrpit
+rm $GITHUB_WORKSPACE/kernel_workspace/android-kernel/scripts/dtc/yamltree.c
+cp $GITHUB_WORKSPACE/patch/scripts/dtc/* $GITHUB_WORKSPACE/kernel_workspace/android-kernel/scripts/dtc
+
+echo add kernelsu
+cd $GITHUB_WORKSPACE/kernel_workspace/android-kernel
+echo "CONFIG_KPROBES=y" >> arch/arm64/configs/${{ github.event.inputs.KERNEL_DEFCONFIG }}
+echo "CONFIG_HAVE_KPROBES=y" >> arch/arm64/configs/${{ github.event.inputs.KERNEL_DEFCONFIG }}
+echo "CONFIG_KPROBE_EVENTS=y" >> arch/arm64/configs/${{ github.event.inputs.KERNEL_DEFCONFIG }}
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
+
+echo change build config
+cd $GITHUB_WORKSPACE/kernel_workspace/android-kernel
+sed -i '/supported.versions.*/d' anykernel.sh
+sed -i '/do.devicecheck.*/d' anykernel.sh
+sed -i 's/KERNEL_NAME=.*/KERNEL_NAME=${{ github.event.inputs.KERNEL_NAME}}-$DATE/g' build.sh
+
+echo build kernel
+cd $GITHUB_WORKSPACE/kernel_workspace/android-kernel
+rm .git -R
+sudo bash build.sh
